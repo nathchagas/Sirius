7adc36cf24a36acf3206de7332c519834b29f7cf
MarianaElma <mariana-elma@hotmail.com>
2020-08-20 11:36:03 -0300

    novo codigo tentativa_2

--- conteudo.php
+++ conteudo.php
@@ -1,7 +1,63 @@
- <section class="espacodesenvolvedor">
+<!-- script de rolagem -->
+<script type="text/javascript">
+ jQuery(document).ready(function($) { 
+     $("#scroll").click(function(event){        
+         event.preventDefault();
+         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
+    });
+ });</script>
+
+<section id="home">
+<div class="home">
+        <div class="centralizador">
+            <img class="centromeio" src="imagens/onglogo.png">
+        </div>
+            <br />
+            <center><a href="#sobre" class="scroll" id="buttonp">CONHEÇA</a></center>
+        </div>
+    </section>
+    
+    <section id="sobre">
+            <div class="espaco">
+                <div class="inicio">
+            <p class="titulo">Sobre nós</p>
+     <hr class="barra">
+                    
+                    </div>
+                
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+    
+ <div class="conteudo">          
+<div class="img_sobre">
+            <img class="pnglogo" src="imagens/pnglogo.png">
+    </div>
+                
+        <div class="textosobre">
+            <div class="curto">
+            <p class="paragrafo">A Sirius Web Design é uma empresa criada para fins didáticos de prestação de serviços para a elaboração e manutenção de sites e páginas web. Criada com o intuito de atender as necessidades do Trabalho de Conclusão  de Curso dos alunos do curso ténico de Informática para Internet Integrado ao Ensino Médio do <b class="verde">Intituto Federal de São Paulo - Campus Guarulhos</b>, estamos abertos a comunidade externa afim de oferecer um trabalho de qualidade. </p>
+            <p class="paragrafo">A respeito da origem do nome de nossa empresa, Sirius é a estrela mais brilhante visível a olho nu no céu noturno e pode ser vista a partir de qualquer ponto na Terra. 
+            Sendo a principal de sua constelação, é muitas vezes apelidada de <b class="verde">Estrela do Cão (Dog-Star)</b>, mas o seu termo Sirius deriva do grego seirios <b class="verde"><i>("brilhante, ou a escaldante")</i></b>. Possui registros remanescentes do Egito Antigo, os quais acreditavam que a mesma possuia grande influência nas estações do ano, homens e mulheres.</p>
+            <p class="paragrafo">Em nossa identidade visual, buscamos representar de maneira simplificada e clara a letra -S de <b class="verde">Sirius</b>. Tendo sobreposta uma estrela acompanhada de um campo gravitacional, a ideia era a de transmitir de maneira rápida a conexão entre nossa empresa e o logo.</p>
+            <p><a href="#desenvolvedores" class="scroll" id="buttonp">Conheça os desenvolvedores</a></p>
+
+        </div>
+
+            </div> 
+
+       <div class="divisor"></div> 
+        </div>
+                </div>     
+
       <div id="desenvolvedores">
           <center>
+             
+              <div class="titulo_dsv">
               <p class="titulo1" ><br/><br/>Conheça nossos Desenvolvedores</p>
+              </div>
               <div id="contatos">
                 <div class="row">
         <!-- div desenvolvedor (João) -->
@@ -41,16 +97,15 @@
             </center>
       </div>
 </section>
-        
-        
+    
 <section class="home">
     <center>
       <div id="servicos">
         <div class="titulo1" ><br/><br/>Nossos Serviços</div>
         <br/><br/>
-        <!-- <div class="direita">  LEVA OS ITENS DO SERVIÇO PARA A DIREITA-->
+       <div class="servicos">
         <div class="row">
-          <div class="column1">
+          <div class="column">
             <div class="card">
                 <div class="containeservicos">
                   <img src="imagens/web.png" width="50px" height="50px">
@@ -60,7 +115,7 @@
              </div>
           </div>
 
-          <div class="column1">
+          <div class="column">
             <div class="card">
                 <div class="containeservicos1">
                   <img src="imagens/servidor.png" width="50px" height="50px">
@@ -70,7 +125,7 @@
             </div>
           </div>
             
-        <div class="column1">
+        <div class="column">
           <div class="card">
             <div class="containeservicos">
              <img src="imagens/manutencao.png" width="50px" height="50px">
@@ -80,86 +135,108 @@
           </div>
         </div>
      </div> <!-- fecha a div "row" -->
+    </div>
     </div> <!-- fecha a div "id serviços" -->
   </center>
 </section>
-        
+
+    
+
 <section class="espaco">
  
-  <div id="conceitos">
-    <div class="sobrepor">
-      <p class="tituloconceito">Nossos<br/>Conceitos
-      <p class="tituloconceitoabaixo">Aquilo que acreditamos e desejamos a nossa empresa está aqui.
-      <br/><br/><a href="#contato" class="scroll" id="buttonp">Contato</a></p>     
-      <br/><br/><br/><br/>
+    <div id="conceitos">
+        <div class="sobrepor">
+      
+          <p class="tituloconceito">Nossos<br/>Conceitos
 
-        <div class="box">
-          <img src="imagens/missao.png" style="width: 60px; height:60px;">
-          <p class="conceitostitulo">Missão</p>
-          <p class="conceitosbox">A nossa missão consiste em desenvolver web sites de extrema qualidade e segurança, afim de proporcionar a nossos clientes um alcance maior em seu negócio.</p>
-       </div>
+          <p class="tituloconceitoabaixo">Aquilo que acreditamos e desejamos a nossa empresa está aqui.
 
-        <div class="box">
-            <img src="imagens/visao.png" style="width: 60px; height:60px;">
-            <p class="conceitostitulo">Visão</p>
-            <p class="conceitosbox">Ser referência na área de desenvolvimento de web sites e serviços web, sempre buscando inovações e soluções tecnologicas, reconhecida pela qualidade na prestação de serviços.</p>
-        </div>
+            <br/>
+            <br/>
+            <a href="#contato" class="scroll" id="buttonp">Contato</a>
+         </p>     
+      
+            <br/>
+            <br/>
+            <br/>
+            <br/>
 
-       <div class="box">
-          <img src="imagens/valores.png" style="width: 60px; height:60px;">
-          <p class="conceitostitulo">Valores</p>
-          <p class="conceitosbox">Ética, respeito e transparência.</p>
-       </div>
-        
-       <div class="divisor1"></div>
-    </div> <!-- fecha a div "sobrepor" -->
-  </div> <!-- fecha a div "id conceitos" -->
+    
+            <div class="box">
+              <img src="imagens/missao.png" style="width: 60px; height:60px;">
+              <p class="conceitostitulo">Missão</p>
+              <p class="conceitosbox">A nossa missão consiste em desenvolver web sites de extrema qualidade e segurança, afim de proporcionar a nossos clientes um alcance maior em seu negócio.</p>
+            </div>
+
+              <div class="box">
+                <img src="imagens/visao.png" style="width: 60px; height:60px;">
+                <p class="conceitostitulo">Visão</p>
+                <p class="conceitosbox">Ser referência na área de desenvolvimento de web sites e serviços web, sempre buscando inovações e soluções tecnologicas, reconhecida pela qualidade na prestação de serviços.</p>
+            </div>
+
+            <div class="box">
+              <img src="imagens/valores.png" style="width: 60px; height:60px;">
+              <p class="conceitostitulo">Valores</p>
+              <p class="conceitosbox">Ética, respeito e transparência.</p>
+            </div>
+
+        </div> <!-- fecha a div sobrepor -->
+
+        <div class="divisor1"></div>
+    </div> <!-- fecha a div conceitos -->
 </section>
 <br />
-        
+
+
 <div id ="navleft">
-<section class="espaco"> 
 
-    <div id="contato">
-        <div class="titulo1"><br/><br/>Contato</div>
-            <div id="uniao_contato">
+  <section class="espaco"> 
 
-             <div class ="formulario">
-                <form action="action_page.php">
+<div id="contato">
 
-                    <label class="titulo_form" for="fname">Nome completo</label>
-                    <input type="text" id="fname" name="nome">
+  <div class="titulo1"><br/><br/>Contato</div>
 
-                    <label class="titulo_form" for="lname">Email</label>
-                    <input type="text" id="lname" name="email" >
 
-                    <label class="titulo_form" for="lname">Assunto</label>
-                    <input type="text" id="lname" name="assunto">
+  <div id="uniao_contato">
+    
+<div class ="formulario">
+<form action="action_page.php">
 
-                    <label class="titulo_form" for="subject">Mensagem</label>
-                    <textarea id="subject" name="subject" style="height:60px"></textarea>
+    <label class="titulo_form" for="fname">Nome completo</label>
+    <input type="text" id="fname" name="nome">
 
-                    <input type="submit" value="Submit">
+    <label class="titulo_form" for="lname">Email</label>
+    <input type="text" id="lname" name="email" >
+    
+    <label class="titulo_form" for="lname">Assunto</label>
+    <input type="text" id="lname" name="assunto">
 
-                </form>
-              </div>   
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.5785476321253!2d-46.53908158538711!3d-23.43958686299075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef51fbe5f8681%3A0x1f8cf929993dc97!2sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%2C%20Ci%C3%AAncia%20e%20Tecnologia%20de%20S%C3%A3o%20Paulo%20IFSP%20Guarulhos!5e0!3m2!1spt-BR!2sbr!4v1584387797174!5m2!1spt-BR!2sbr" width="50%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> <!-- mapa com o endereço do IF -->
+    <label class="titulo_form" for="subject">Mensagem</label>
+    <textarea id="subject" name="subject" style="height:60px"></textarea>
 
-            </div> <!-- fecha a div "id uniao_contato -->
-      </div> <!-- fecha a div "id contato" -->
-</section>
-</div>
-        
+    <input type="submit" value="Submit">
+
+  </form></div>
+
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.5785476321253!2d-46.53908158538711!3d-23.43958686299075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef51fbe5f8681%3A0x1f8cf929993dc97!2sInstituto%20Federal%20de%20Educa%C3%A7%C3%A3o%2C%20Ci%C3%AAncia%20e%20Tecnologia%20de%20S%C3%A3o%20Paulo%20IFSP%20Guarulhos!5e0!3m2!1spt-BR!2sbr!4v1584387797174!5m2!1spt-BR!2sbr" width="50%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+</div></div>
+</section></div>
+
+    
  <div id ="footer"> 
+
      <footer class="fixarrodape"><br/>
        <img src="imagens/pnglogo.png" width="50px" height="50px"><br/>
-            Todos os direitos reservados.
-            <br/>
-            Created by © Sirius Web Design
-         <div class="w3-xlarge w3-section"> <!-- div que contem os icones das redes sociais --> 
-             <a href="https://www.facebook.com/siriusdesignweb/" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
-             <a href="https://www.instagram.com/siriuswebdesign/?hl=pt-br" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
-             <a href="https://twitter.com/siriusdesignweb" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
-          </div>
-      </footer>
+Todos os direitos reservados.
+<br/>
+Created by © Sirius Web Design
+<div class="w3-xlarge w3-section">
+ <a href="https://www.facebook.com/siriusdesignweb/" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+ <a href="https://www.instagram.com/siriuswebdesign/?hl=pt-br" target="_blank"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+ <a href="https://twitter.com/siriusdesignweb" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+</div>
+</footer>
+
+
 </div>
---
+++ header2.php
@@ -0,0 +1,77 @@
+<div class="topnav" id="myTopnav">
+<nav class="navbar navbar-expand-md fixed-top navbar-dark" id="navbar">  <!-- para deixar transparente (bg-transparent) -->
+  <!-- Botão que aparece quando a página está com o tamnho reduzido -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <!-- (mx-auto) centraliza o menu -->
+  
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav mx-auto"> 
+      <li class="nav-item">
+          <a href="#sobre" class="nav-link" id="scroll"><p id="fonte"> Sobre nós</p></a>
+      </li>
+      <li class="nav-item">
+          <a href="#servicos" class="nav-link" id="scroll" ><p id="fonte">Serviços</p></a>
+      </li>
+      <li class="nav-item">
+        <a href="#home" class="nav-link" id="scroll" ><img src="imagens/logo_sirius.gif" height="40"></a>
+      </li>
+        <li class="nav-item">
+            <a href="#conceitos" class="nav-link" id="scroll" ><p id="fonte">Conceitos</p></a>
+      </li>
+        <li class="nav-item">
+            <a href="#contato" class="nav-link" id="scroll" ><p id="fonte">Contato</p></a>
+      </li>
+    </ul>
+  </div>  
+    
+    
+    
+</nav>
+</div>
+
+
+<style type="text/css"> 
+    #fonte {
+        font-size: 20px;
+        color:#00d0c5;
+    }
+    
+    
+.mudaCor{
+   background-color:#fff;
+
+    }
+</style>
+
+<!-- script de rolagem -->
+<script type="text/javascript">
+ jQuery(document).ready(function($) { 
+     $("#scroll").click(function(event){        
+         event.preventDefault();
+         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
+    });
+ });</script>
+
+
+<!-- Script que muda a cor do menu de transparente para branco -->
+<script type="text/javascript">
+    var el = document.getElementById('navbar'); // elemento alvo
+    var numPx = '250'; // Quantidade de pixels a contar do TOP até definir a cor
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > numPx) {
+            el.classList.add('mudaCor');// adiciona classe "mudaCor"
+        } else {
+          el.classList.remove('mudaCor'); // remove classe "mudaCor"
+        }
+    });
+    </script>
+
+
+
+
+
+
